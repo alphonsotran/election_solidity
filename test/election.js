74149fc4ab133df5eebef018d37d7554945fe9d8
alphonsotran <alphonsotran@gmail.com>
2018-05-28 16:15:42 +0200

    Initialize and write first test in election.js

---
+++ test/election.js
@@ -0,0 +1,13 @@
+var Election = artifcats.require("./Election.sol");
+
+contract("Election", function(accounts){
+
+    it("initiliazes with two candidates", function () {
+        return Election.deployed().then(function(instance) {
+            return instance.candidatesCount();
+        }).then(function(count){
+            assert.equal(count, 2);
+        });
+    });
+
+});
