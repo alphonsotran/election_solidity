710f01379a3a215e9d34b30275fa1c608f7fe8ff
alphonsotran <alphonsotran@gmail.com>
2018-05-28 16:59:34 +0200

    Add voters in Election contract and change version number.

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,17 +1,25 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.20;
 
 contract Election {
     // model a candidate
     struct Candidate {
         uint id;
         string name;
-        uint votecount;
+        uint voteCount;
     }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
     // store candidates
     // fetch candidate
     mapping(uint => Candidate) public candidates;
     // store candidates count. can't use a "hash.count". must use counter. 
     uint public candidatesCount;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
 
     function Election () public {
       addCandidate("Candidate 1");
@@ -21,5 +29,22 @@ contract Election {
     function addCandidate (string _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        votedEvent(_candidateId);
     }
 }
