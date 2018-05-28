71f6c1fdeab618785d84f2cad84b9eba087eb3c7
alphonsotran <alphonsotran@gmail.com>
2018-05-28 15:32:55 +0200

    Add code to start Election contract.

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.11;
+
+contract Election {
+    // store candidate 
+    // read candidate as getter with "public"
+    string public candidate; 
+
+    function Election () public {
+        candidate = "Candidate 1";
+    }
+}
